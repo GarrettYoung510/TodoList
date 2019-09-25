bbe776bdf3bc4557843722b49900184ad449241c
Garrett Young <garrettyoung510@gmail.com>
2019-09-24 19:50:41 -0700

    delete todos capability

--- app.js
+++ app.js
@@ -19,9 +19,18 @@ addForm.addEventListener("submit", e => {
   // use .trim() for no white spaces
   const todo = addForm.add.value.trim();
 
-//   check if todo has length
+  //   check if todo has length
   if (todo.length) {
     //   then run todo within the generate template
     generateTemplate(todo);
+  }
+});
+
+// delete todo
+list.addEventListener("click", e => {
+  // if clicked element with class with delete in it
+  if (e.target.classList.contains("delete")) {
+    // deletes parent element
+    e.target.parentElement.remove();
   }
 });
